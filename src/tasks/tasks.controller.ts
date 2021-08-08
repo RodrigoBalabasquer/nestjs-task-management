c0f2f73fab96f176a80584387566dbfb1f68af8a
RodrigoBalabasquer <r.balabasquer@gmail.com>
2021-08-08 19:38:10 -0300

    agrego seccion 3 del curso

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -39,8 +40,9 @@ export class TasksController {
     @Put('/:id/status')
     updateTaskStatus(
         @Param('id') id: string,
-        @Body('status') status: TaskStatus
+        @Body() updateTaskStatus: UpdateTaskStatusDto
     ): Task {
+        const { status } = updateTaskStatus;
         return this.tasksService.updateTaskStatus(id,status);
     }
 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class TasksService {
@@ -33,11 +34,18 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        const found = this.tasks.find((task) => task.id === id);
+
+        if(!found)
+        {
+            throw new NotFoundException('Task with Id '+id+' no found');
+        }
+        return found;
     }
 
     deleteTaskById(id: string): void {
-        this.tasks = this.tasks.filter((task) => task.id != id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task) => task.id != found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus){
